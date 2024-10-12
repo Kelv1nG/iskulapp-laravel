d462dc7505f7c6931272609583b1d0751cef2065
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-12 13:33:11 +0800

    update academic year seeder

--- src/database/seeders/AcademicYearSeeder.php
+++ src/database/seeders/AcademicYearSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\AcademicYear;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class AcademicYearSeeder extends Seeder
@@ -13,10 +14,10 @@ class AcademicYearSeeder extends Seeder
     public function run(): void
     {
         $academicYears = [
-            ['name' => '2023', 'start' => 'Jan', 'end' => 'June'],
-            ['name' => '2023', 'start' => 'July', 'end' => 'Dec'],
-            ['name' => '2024', 'start' => 'Jan', 'end' => 'June'],
-            ['name' => '2024', 'start' => 'July', 'end' => 'Dec'],
+            ['name' => '2022-2023', 'start' => Carbon::create(2022, 1, 1), 'end' => Carbon::create(2022, 6, 30)],
+            ['name' => '2022-2023', 'start' => Carbon::create(2022, 7, 1), 'end' => Carbon::create(2022, 12, 31)],
+            ['name' => '2023-2024', 'start' => Carbon::create(2023, 1, 1), 'end' => Carbon::create(2023, 6, 30)],
+            ['name' => '2023-2024', 'start' => Carbon::create(2023, 7, 1), 'end' => Carbon::create(2023, 12, 31)],
         ];
 
         AcademicYear::insert($academicYears);
