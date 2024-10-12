59305be7bf2e9c7e6398ea7f5e89dfcda69c4a2c
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-12 11:06:51 +0800

    create assessment related models

---
+++ src/app/Models/SubjectYear.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SubjectYear extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['academic_year_id', 'subject_id'];
+
+    public function academicYear(): BelongsTo
+    {
+        return $this->belongsTo(AcademicYear::class, 'academic_year_id', 'id');
+    }
+
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class, 'subject_id', 'id');
+    }
+}
