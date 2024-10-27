4bed3e40ed690653239e2a2d8f37bbf43ec2ccc7
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-27 18:34:17 +0800

    add login type / id in user resource

--- src/app/Http/Resources/User/UserWithRelatedInfoResource.php
+++ src/app/Http/Resources/User/UserWithRelatedInfoResource.php
@@ -16,6 +16,8 @@ class UserWithRelatedInfoResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'login_type' => $this->getRoleNames()->first(),
+            'login_id' => $this->getLoginId(),
             'first_name' => $this->userProfile->first_name,
             'last_name' => $this->userProfile->last_name,
             'email' => $this->email,
@@ -24,5 +26,17 @@ class UserWithRelatedInfoResource extends JsonResource
             'gender' => optional($this->userProfile)->gender,
             'role' => $this->getRoleNames()->first(),
         ];
+    }
+
+    /**
+     * Get the login ID based on user type (teacher or student).
+     */
+    protected function getLoginId(): ?string
+    {
+        return match ($this->getRoleNames()->first()) {
+            'teacher' => $this->teacher?->id,
+            'student' => $this->student?->id,
+            default => null
+        };
     }
 }
