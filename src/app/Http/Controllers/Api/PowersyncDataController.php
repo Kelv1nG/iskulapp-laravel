6e60e0f2a7040f12e696208e5ed8e4b04b57ef0d
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-28 14:42:24 +0800

    add powersync data upload controller

--- src/app/Http/Controllers/Api/PowersyncDataController.php
+++ src/app/Http/Controllers/Api/PowersyncDataController.php
@@ -1,22 +1,32 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Api;
 
+use App\Constants\HttpStatus;
 use App\DataHandlers\DataHandler;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
-class PowersyncDataController extends Controller
+class PowersyncDataController extends ApiController
 {
     public function uploadData(Request $request)
     {
+        try {
+            $method = $request->method();
+            $json = $request->json()->all();
+            $table = $json['table'];
+            $data = $json['data'];
+            DataHandler::handle($table, $method, $data);
 
-        $method = $request->method();
+            return $this->response(HttpStatus::HTTP_OK);
+        } catch (\Exception $e) {
+            Log::error('PowerSync data upload failed: '.$e->getMessage());
 
-        $data = $request->json()->all();
-        $table = $data['table'];
-        $itemData = $data['data'];
-
-        $result = DataHandler::handle($table, $method, $data);
+            return $this->errorResponse(
+                HttpStatus::HTTP_INTERNAL_SERVER_ERROR,
+                'Failed to upload data',
+                $e->getMessage()
+            );
+        }
     }
 }
