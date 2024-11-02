cc89bdc880f157b81e85b2eb278e2eb6ea7d0322
Kelv1nG <kelvinmaglipon@gmail.com>
2024-11-03 07:11:18 +0800

    update models

--- src/app/Models/Student.php
+++ src/app/Models/Student.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Student extends Model
 {
     use HasFactory;
-    use HasUuids;
 
     protected $fillable = ['user_id', 'student_no'];
+
+    public function academicYears(): BelongsToMany
+    {
+        return $this->belongsToMany(AcademicYear::class, 'student_year', 'student_id', 'academic_year_id');
+    }
 }
