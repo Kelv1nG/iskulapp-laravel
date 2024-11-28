84f42b4c9839175de4717de1d4712f3659fdc68e
Kelv1nG <kelvinmaglipon@gmail.com>
2024-11-28 20:30:10 +0800

    add subject seeder

--- src/database/seeders/SubjectSeeder.php
+++ src/database/seeders/SubjectSeeder.php
@@ -4,22 +4,55 @@ namespace Database\Seeders;
 
 use App\Models\Subject;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SubjectSeeder extends Seeder
 {
+    const BASE_SUBJECTS = [
+        'math',
+        'physcial_education',
+        'religion',
+        'english',
+        'music',
+        'sex_education',
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $subjects = [
-            ['name' => 'math'],
-            ['name' => 'physical_education'],
-            ['name' => 'religion'],
-            ['name' => 'english'],
-            ['name' => 'music'],
-        ];
+        $subjectRecords = $this->prepareSubjectRecords();
+        Subject::insert($subjectRecords);
+    }
 
-        Subject::insert($subjects);
+    /*
+    * create subjects for each grade level i.e math-grade_1, math-grade_2
+    */
+    private function prepareSubjectRecords(): array
+    {
+        $results = DB::select('
+            SELECT
+                gl.id AS grade_level_id,
+                gl.name AS grade_level_name,
+                schools.id AS school_id,
+                schools.short_name AS school_short_name
+            FROM grade_levels AS gl
+            LEFT JOIN schools ON schools.id = gl.school_id
+        ');
+
+        $subjectRecords = [];
+        foreach ($results as $result) {
+            foreach (self::BASE_SUBJECTS as $subject) {
+                $subjectRecords[] = [
+                    'name' => "{$subject}-{$result->grade_level_name}-{$result->school_short_name}",
+                    'school_id' => $result->school_id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+        }
+
+        return $subjectRecords;
     }
 }
