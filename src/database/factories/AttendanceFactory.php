e9c13925df0fddddc282eccd59ef4cece8d7cc16
Kelv1nG <kelvinmaglipon@gmail.com>
2024-12-12 09:01:55 +0800

    attendance factory

---
+++ src/database/factories/AttendanceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AcademicYear;
+use App\Models\Student;
+use App\Models\Teacher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Attendance>
+ */
+class AttendanceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'student_id' => Student::factory(),
+            'checked_by' => Teacher::factory(),
+            'academic_year_id' => AcademicYear::factory(),
+            'attendance_date' => $this->faker->date(),
+            'time_in' => $this->faker->optional()->time(),
+            'is_absent' => $this->faker->boolean,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
